97c10b8f5d3273cdabf2c75c95b4c6f973a0888a
kimyc3825 <35181518+kimyc3825@users.noreply.github.com>
2019-08-18 23:02:52 +0900

    Added getATR method
    Added getATR Method

---
+++ atr.py
@@ -0,0 +1,21 @@
+import pybithumb
+
+currency = "BTC"
+interval = "minute"
+
+
+
+def getATR(currency, interval):
+    df = pybithumb.get_ohlcv(currency, interval)
+    close = df['close'][-15:-1]
+    high = df['high'][-15:-1]
+    low = df['low'][-15:-1]
+    
+    TRs = []
+    TRs.append(high[0]-low[0])
+    for i in range(1,14):
+        TRs.append(max(abs(high[i]-low[i]), abs(high[i]-close[i-1]), abs(low[i]-close[i-1])))
+    return sum(TRs) / len(TRs)
+    
+
+print(getATR(currency, interval))
--- loop.py
+++ loop.py
@@ -20,7 +20,7 @@ def aboveline(currency, interval):
         return 1
     else:
         return 0
-#최종적으로는, 현재가격이 15일 이평선보다 1ATR만큼 아래일 때
+#최종적으로는, 현재가격이 15일 이평선보다 1ATR만큼 아래일 때, 추가해야할것 : 현재 atr을 구해서 이평선에서 빼준다. 그값을 프라이스나우와 비교.
 def belowline(currency, interval):
     priceNow = pybithumb.get_current_price(currency)
     df = pybithumb.get_ohlcv(currency, interval)
@@ -29,7 +29,18 @@ def belowline(currency, interval):
         return 1
     else:
         return 0
+
+def getATR(currency, interval):
+    df = pybithumb.get_ohlcv(currency, interval)
+    close = df['close'][-15:-1]
+    high = df['high'][-15:-1]
+    low = df['low'][-15:-1]
     
+    TRs = []
+    TRs.append(high[0]-low[0])
+    for i in range(1,14):
+        TRs.append(max(abs(high[i]-low[i]), abs(high[i]-close[i-1]), abs(low[i]-close[i-1])))
+    return sum(TRs) / len(TRs)
 
 def buy(currency, units):
     print("buy")
@@ -47,6 +58,7 @@ positioned = False
 con_key = ""
 sec_key = ""
 balance = 0
+buy_price = 0
 #----------------------------------
 while True:
     if marketReader(currency, interval) and positioned == False:
